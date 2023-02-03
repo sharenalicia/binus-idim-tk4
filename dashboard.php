497fbd45cdf66b9cdfbe626345e21e0a3a9a5e38
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-03 22:34:03 +0700

    dashboard 3

--- dashboard.php
+++ dashboard.php
@@ -94,36 +94,29 @@ if (!isset($_SESSION['idpengguna'])){
                 <section class="content-1">
                     <div class="box-1">
                         <div class="box-icon1">
-                            <h2> Jumlah Penjualan </h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 96c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/></svg>    
                         </div>
 
                         <div class="box-content1">
-                            <h2> angka db </h2>
+                            <span class="big">angkadb</span>
+						    Barang Terjual
                         </div>
                     </div>
                     <div class="box-1">
                         <div class="box-icon1">
-                            <h2> Jumlah Penjualan </h2>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 96c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/></svg>    
                         </div>
 
                         <div class="box-content1">
-                            <h2> angka db </h2>
+                            <span class="big">angkadb</span>
+						    Barang Terjual
                         </div>
                     </div>
                 </section>
                 <section class="content-2">
                     <div class="box-2">
                         <div class="box-icon2">
-                            <h2> Jumlah Penjualan </h2>
-                        </div>
-
-                        <div class="box-content2">
-                            <h2> angka db </h2>
-                        </div>
-                    </div>
-                    <div class="box-2">
-                        <div class="box-icon2">
-                            <h2> Jumlah Penjualan </h2>
+                            <h2> Laporan Laba Rugi </h2>
                         </div>
 
                         <div class="box-content2">
