b668eea55d69693c1e95a2168a1ad3bf6991b9de
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-05 21:46:12 +0700

    index

--- index.php
+++ index.php
@@ -13,9 +13,7 @@
 
     <body>
     <?php
-        $IdPengguna = $_SESSION['IdPengguna'];
         $IdAkses = $_SESSION['IdAkses'];
-        $Keterangan = $_SESSION['Keterangan'];
         $query = mysqli_query($connect, "SELECT IdPengguna, IdAkses, Keterangan FROM pengguna JOIN hakakses USING (IdAkses)");
         $p = mysqli_fetch_array($query);
         if($Keterangan=='Admin'){
