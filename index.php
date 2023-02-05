01efd935ed1ae264d6db43229dfce3d87f736ad3
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-05 21:32:22 +0700

    edit hak akses login

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         $IdPengguna = $_SESSION['IdPengguna'];
         $IdAkses = $_SESSION['IdAkses'];
         $Keterangan = $_SESSION['Keterangan'];
-        $query = mysqli_query($connect, "SELECT IdPengguna, IdAkses, Keterangan FROM pengguna JOIN hakakses USING ('IdAkses')");
+        $query = mysqli_query($connect, "SELECT IdPengguna, IdAkses, Keterangan FROM pengguna JOIN hakakses USING (IdAkses)");
         $p = mysqli_fetch_array($query);
         if($Keterangan=='Admin'){
             echo "<meta http-equiv='refresh' content='0 url=dashboard.php'>";
