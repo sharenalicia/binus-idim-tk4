de5fa0e03d648dd1c2a581bb9fc0ab416dde862e
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-05 21:27:03 +0700

    edit hak akses

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-header("Location: dashboard.php");
-exit();
+    include "koneksi.php";
+    session_start();
+    if (!isset($_SESSION['IdPengguna'])){
+        header ("location:login.php");
+    }
+?>
+
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+    <?php
+        $IdPengguna = $_SESSION['IdPengguna'];
+        $IdAkses = $_SESSION['IdAkses'];
+        $Keterangan = $_SESSION['Keterangan'];
+        $query = mysqli_query($connect, "SELECT IdPengguna, IdAkses, Keterangan FROM pengguna JOIN hakakses USING ('IdAkses')");
+        $p = mysqli_fetch_array($query);
+        if($Keterangan=='Admin'){
+            echo "<meta http-equiv='refresh' content='0 url=dashboard.php'>";
+        }else if($Keterangan=='User'){
+            echo "<meta http-equiv='refresh' content='0 url=dashboard_view.php'>";
+        }
+    ?>
+    </body>
+
+</html>
