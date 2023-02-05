b7675d7ec5300c47773eb7eae17e803b14661e9a
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-05 22:23:30 +0700

    masih index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include "koneksi.php";
+    include "koneksidb.php";
     session_start();
     if (!isset($_SESSION['IdPengguna'])){
         header ("location:login.php");
@@ -13,10 +13,10 @@
 
     <body>
     <?php
-        $IdAkses = $_SESSION['IdAkses'];
-        $Keterangan = $_SESSION['Keterangan'];
-        $query = mysqli_query($connect, "SELECT IdPengguna, IdAkses, Keterangan FROM pengguna JOIN hakakses WHERE IdAkses = '$IdAkses'");
+        $IdPengguna = $_SESSION['IdPengguna'];
+        $query = mysqli_query($connect, "SELECT * FROM pengguna JOIN hakakses USING (IdAkses)");
         $p = mysqli_fetch_array($query);
+        $Keterangan = $p['Keterangan'];
         if($Keterangan=='Admin'){
             echo "<meta http-equiv='refresh' content='0 url=dashboard.php'>";
         }else if($Keterangan=='User'){
