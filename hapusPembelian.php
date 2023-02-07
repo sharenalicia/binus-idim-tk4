4a3abb6cc95b21986101ba335ea011961821a186
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2023-02-07 23:26:02 +0700

    hapus pembelian

--- hapusPembelian.php
+++ hapusPembelian.php
@@ -1,11 +1,22 @@
 <?php
-include 'koneksidb.php';
-$IdPembelian = $_GET['IdPembelian'];
-$query = "DELETE FROM pembelian WHERE IdPembelian='$IdPembelian'";
-$sql = mysqli_query($connect, $query);
-if ($sql) {
-    header("location: dataPembelian.php");
-} else {
-    echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-    echo "<br><a href='dataPembelian.php'>Kembali Ke Form</a>";
+include_once("DBController.php");
+$db_handle = new DBController();
+$IdPembelian = $_GET['IdIdPembelian'];
+
+if (!empty($IdIdPembelian)) {
+    $query = "DELETE FROM pembelian_d WHERE `pembelian_d`.`IdPembelian` = '" . $_GET["IdPembelian"] . "'";
+    $query2 = "DELETE FROM pembelian_h WHERE `pembelian_h`.`IdPembelian` = '" . $_GET["IdPembelian"] . "'";
+    try {
+        $result = $db_handle->deleteQuery($query);
+        $result2 = $db_handle->deleteQuery($query2);
+        if (!empty($result) && !empty($result2)) {
+            header("Location:detailPembelian.php");
+        } else {
+            echo "<script>alert('Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.')</script>";
+            echo "<meta http-equiv='refresh' content='0; url=dataBarang.php'>";
+        }
+    } catch (Exception $e) {
+        echo "<script>alert('Internal Server Error')</script>";
+        echo "<meta http-equiv='refresh' content='0; url=dataBarang.php'>";
+    }
 }
